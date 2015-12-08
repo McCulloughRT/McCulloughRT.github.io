edc19b44accac99853c1bc737fc0e0f0f286f1b8
McCulloughRT <McCulloughRT@gmail.com>
2015-12-07 23:59:10 -0800

    fixed resize problem

--- rvt3js/js/THREEx.WindowResize.js
+++ rvt3js/js/THREEx.WindowResize.js
@@ -6,7 +6,7 @@
 // **Step 1**: Start updating renderer and camera
 //
 // ```var windowResize = THREEx.WindowResize(aRenderer, aCamera)```
-//    
+//
 // **Step 2**: Start updating renderer and camera
 //
 // ```windowResize.stop()```
@@ -15,30 +15,31 @@
 //
 
 /** @namespace */
-var THREEx	= THREEx 		|| {};
+var THREEx = THREEx || {};
 
 /**
  * Update renderer and camera when the window is resized
- * 
+ *
  * @param {Object} renderer the renderer to update
  * @param {Object} Camera the camera to update
-*/
-THREEx.WindowResize	= function(renderer, camera){
-	var callback	= function(){
-		// notify the renderer of the size change
-		renderer.setSize( window.innerWidth, window.innerHeight );
-		// update the camera
-		camera.aspect	= window.innerWidth / window.innerHeight;
-		camera.updateProjectionMatrix();
-	}
-	// bind the resize event
+ */
+THREEx.WindowResize = function(renderer, camera) {
+	var callback = function() {
+			// notify the renderer of the size change
+			renderer.setSize(window.innerWidth, window.innerHeight);
+			// update the camera
+			camera.aspect = window.innerWidth / window.innerHeight;
+			camera.updateProjectionMatrix();
+			render();
+		}
+		// bind the resize event
 	window.addEventListener('resize', callback, false);
 	// return .stop() the function to stop watching window resize
 	return {
 		/**
 		 * Stop watching window resize
-		*/
-		stop	: function(){
+		 */
+		stop: function() {
 			window.removeEventListener('resize', callback);
 		}
 	};
